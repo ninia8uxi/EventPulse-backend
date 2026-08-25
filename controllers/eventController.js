7475bffc2e5f634e1ec4b93c2fff75267a45de65
ninia8uxi <yusrykira@gmail.com>
2026-08-25 07:04:43 +0300

    fix createEvent

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -144,4 +144,12 @@ const getEvents = asyncHandler(async (req, res, next) => {
     next(error);
   }
 });
+const createEvent = async (req, res) => {
+  try {
+    const event = await Event.create(req.body);
+    res.status(201).json(event);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 module.exports = { getEvents, createEvent };
