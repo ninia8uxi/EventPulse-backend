dda7fd06b294b8bc4f351d614508bf1f09b77215
ninia8uxi <yusrykira@gmail.com>
2026-08-25 06:52:28 +0300

    remove app.listen

--- server.js
+++ server.js
@@ -38,12 +38,4 @@ app.use((err, req, res, next) => {
     message: err.message
   });
 });
-
-if (process.env.NODE_ENV !== 'test') {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-  });
-}
-
 module.exports = app;
